7b265dab3215315a45c595357664e9699815bdbb
Amy Huang <amyxjhuang@berkeley.edu>
2019-03-27 17:35:10 -0700

    using user input

--- app.py
+++ app.py
@@ -1,34 +1,23 @@
 from flask import Flask, render_template
+import brotherspot
+import requests
 
 app = Flask(__name__)
-locations = ["blackwell", "unit 1", "unit 2", "unit 3",
-            "crossroads", "cafe 3", "moffitt", "mainstacks",
-            "dwinelle", "ucha", "gong cha", "sather lane",
-            "kresge", "evans", "leconte", "asian ghetto",
-            "stanley", "soda", "cory", "etcheverry",
-            "tause", "rsf", "foothill", "upper sproul", "mlk",
-            "eshelmann", "sproul", "lower sproul",
-            "wheeler", "doe", "latimer", "trader joes",
-            "etch", "esh", "cafe blue door", "gbc", "sdh",
-            "edwards stadium", "daiso", "sharetea", "chipotle"]
 
-brotherLoc = {}
 @app.route("/")
 def index():
-    userInput = ["brother", "blackwell", 1200];
-    uName = userInput[0]
-    uLoc = userInput[1]
-    uTime = userInput[2]
-    if uLoc in locations:
-        brotherLoc[uName] = [uLoc, uTime]
-        return "Added: " + uName + " has been spotted at " +
-                uLoc + " at " + str(uTime)
-    else:
-        return notInLoc(uLoc)
+    return "this is a program"
+
+@app.route("/spotted", methods= ['POST'])
+def spotted():
+    raw_text = request.form.get('text')
+    text_array = re.findall(r'["“”‘’\'](.*?)["“”‘’\']', raw_text)
+    if len(text_array) != 3:
+        return 'The format is /spotted "[name]" "[location]" "[time]"'
+    title, start, end = text_array
+    return brotherspot.getInfo(text_array)
 
 
-def notInLoc(str s):
-    return "location is not in list"
 
 if __name__ == "__main__":
     app.run()
--- brotherspot.py
+++ brotherspot.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 import requests
+import json
 
-ocations = ["blackwell", "unit 1", "unit 2", "unit 3",
+url = "https://hooks.slack.com/services/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"
+locations = ["blackwell", "unit 1", "unit 2", "unit 3",
             "crossroads", "cafe 3", "moffitt", "mainstacks",
             "dwinelle", "ucha", "gong cha", "sather lane",
             "kresge", "evans", "leconte", "asian ghetto",
@@ -15,15 +17,16 @@ ocations = ["blackwell", "unit 1", "unit 2", "unit 3",
 brotherLoc = {}
 
 def getInfo(userInput):
-    userInput = ["brother", "blackwell", 1200];
-    uName = userInput[0]
-    uLoc = userInput[1]
-    uTime = userInput[2]
-    if uLoc in locations:
-        brotherLoc[uName] = [uLoc, uTime]
-        chat.postMessage("Added: " + uName + " has been spotted at " + uLoc + " at " + str(uTime))
+    # userInput = ["brother", "blackwell", "12:00"];
+    name = userInput[0]
+    loc = userInput[1]
+    time = userInput[2]
+    if loc in locations:
+        brotherLoc[name] = [loc, time]
+        output = "Added: " + name + " has been spotted at " + loc + " at " + str(time)
+        # output = uName;
     else:
-        return notInLoc(uLoc)
-
-def notInLoc(s):
-    chat.postMessage("location is not in list")
+        # return notInLoc(uLoc)
+        output = "Location is not in list"
+    data = {"text": output}
+    requests.post(url, data = json.dumps(data))
